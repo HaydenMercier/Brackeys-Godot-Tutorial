2483e0c16a19456eba5a357703a1dd4b846b6bbb
UltraArcher <ultra.archerhm@gmail.com>
2024-07-26 21:33:13 +1000

    updated the map and started working on a coyotetime mechanic, doesnt work yet.

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -1,31 +1,35 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -320.0
-@onready var animated_sprite = $AnimatedSprite2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_coyote_jump = false
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
 
 func _ready():
-	teleport_to_spawn_point()
+	return
 
 func _physics_process(delta):
-	if not is_on_floor():
+	if not is_on_floor() && !can_coyote_jump:
 		velocity.y += gravity * delta
 
+	if Input.is_action_just_pressed("jump"):
+		if can_coyote_jump || is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			if can_coyote_jump:
+				can_coyote_jump = false
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
-	
+	print("Direction: ", direction)  # Debug statement to check direction input
+
 	if direction > 0:
 		animated_sprite.flip_h = false
-	elif direction <0:
+	elif direction < 0:
 		animated_sprite.flip_h = true
+
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -34,14 +38,21 @@ func _physics_process(delta):
 	else:
 		animated_sprite.play("jump")
 
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = direction * SPEED
+
+	print("Velocity X: ", velocity.x)  # Debug statement to check velocity
 
 	move_and_slide()
+
+	var was_on_floor = is_on_floor()
+	if was_on_floor && !is_on_floor():
+		coyote_timer.start()
+		can_coyote_jump = true
 
 func teleport_to_spawn_point():
 	if $SpawnPoint:
 		position = $SpawnPoint.global_position
 		velocity = Vector2.ZERO
+
+func _on_coyote_timer_timeout():
+	can_coyote_jump = false
